2c31978ca716bcf77ca89e126d822809e05fa1c4
TheDeveloperIsDhruv <108569661+TheDeveloperIsDhruv@users.noreply.github.com>
2025-02-25 02:35:53 -0500

    Done with SignIn page

--- signin.php
+++ signin.php
@@ -1,7 +1,25 @@
 <?php
-echo "LoginID is: ";
-print_r($_POST['loginId']);
-echo "<br>";
-echo "Password is: ";
-print_r($_POST['passwd']);
+
+$conn=mysqli_connect("localhost","root","","OSP");
+if(!$conn){
+  
+    die(mysqli_connect_error());
+}
+else{
+    
+}
+$sql="Select Login_ID,Password from User where Login_ID=? and Password=?";;
+$stmt=$conn->prepare(($sql));
+$stmt->bind_param("ss",$_GET['loginID'],$_GET['passwd']);
+$stmt->execute();
+$res=$stmt->get_result();
+
+if ($res->num_rows>0){
+  $response="Success!";
+}
+else{
+   $response="Failed!";
+}
+echo $response;
+
 ?>
