8b50ea380490262572c6635c191bf53c7e0dfcc1
kirtan <115176374+kirtan10369@users.noreply.github.com>
2025-03-03 19:10:54 -0500

    added admin part with php

--- signin.php
+++ signin.php
@@ -1,25 +1,32 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-$conn=mysqli_connect("localhost","root","","OSP");
-if(!$conn){
-  
+$conn = mysqli_connect("localhost", "root", "", "OSP");
+if (!$conn) {
     die(mysqli_connect_error());
 }
-else{
-    
-}
-$sql="Select Login_ID,Passwd from User where Login_ID=? and Passwd=?";;
-$stmt=$conn->prepare(($sql));
-$stmt->bind_param("ss",$_GET['loginID'],$_GET['passwd']);
+
+$loginId = $_GET['loginID'] ?? '';
+$passwd = $_GET['passwd'] ?? '';
+$sql = "SELECT Login_ID, Passwd FROM User WHERE Login_ID = ? AND Passwd = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $loginId, $passwd);
 $stmt->execute();
-$res=$stmt->get_result();
+$res = $stmt->get_result();
 
-if ($res->num_rows>0){
-  $response="Success!";
+if ($res->num_rows > 0) {
+    echo "Success!";
+    // Set loggedIn and redirect based on user
+    if ($loginId === "admin") {
+        echo "<script>localStorage.setItem('loggedIn', 'admin'); window.location.href = 'admin.html';</script>";
+    } else {
+        echo "<script>localStorage.setItem('loggedIn', '$loginId'); window.location.href = 'shopping.html';</script>";
+    }
+} else {
+    echo "Failed!";
 }
-else{
-   $response="Failed!";
-}
-echo $response;
 
+$stmt->close();
+$conn->close();
 ?>
